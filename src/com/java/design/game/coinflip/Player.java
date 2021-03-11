c121a7c3f8e00b1647a266248531bcd77daf4417
sidkrish03 <ss3875@njit.edu>
2021-03-11 13:18:43 -0500

    Initial Commit: Adding Player class to get random coin option(Heads or Tails) for the player, determine winner

---
+++ src/com/java/design/game/coinflip/Player.java
@@ -0,0 +1,41 @@
+package com.java.design.game.coinflip;
+
+public class Player {
+    private String name = "";
+    private String coinOption = "";
+
+    public String[] coinValue = {"Heads", "Tails"};
+
+    Player(String newName) {
+        name = newName;
+    }
+
+    public String getCoinOption() {
+        return coinOption;
+    }
+
+    // Set coinOption to the opposite of what is set for player 1
+    public void setCoinOption(String opponentFlip) {
+        coinOption = (opponentFlip == "Heads") ? "Tails" : "Heads";
+    }
+
+    public String getRandCoinOption() {
+        // Get random option 0  or 1
+        int randNum = (Math.random() < 0.5) ? 0 : 1;
+
+        // Set value based on the chosen random value
+        // for the array coinValue which will then choose either head or tails
+        coinOption = coinValue[randNum];
+
+        return coinValue[randNum];
+    }
+
+    // Decide the winner
+    public void didPlayerWin(String winningFlip) {
+        if(coinOption == winningFlip) {
+            System.out.println(name + " won with a flip side of " + coinOption);
+        } else {
+            System.out.println(name + " lost with a flip side " + coinOption);
+        }
+    }
+}
