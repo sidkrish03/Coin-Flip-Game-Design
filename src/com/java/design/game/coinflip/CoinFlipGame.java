e5d31dbb6f803f600be7db3b5e643cfeb2260af7
sidkrish03 <ss3875@njit.edu>
2021-03-11 13:22:14 -0500

    Initial Commit: Adding CoinFlipGame class to play the game until user chooses to

---
+++ src/com/java/design/game/coinflip/CoinFlipGame.java
@@ -0,0 +1,20 @@
+package com.java.design.game.coinflip;
+import java.util.Scanner;
+
+public class CoinFlipGame {
+    public static void main(String[] args) {
+        // Create a coin game with the 2 players provided
+        CoinFlip newCoinGame = new CoinFlip("Sid", "Jack");
+
+        // Response from the user to either continue or end game
+        String usersResponse;
+
+        do {
+            newCoinGame.startGame();
+            System.out.println("Play Again? ");
+            System.out.println("Please give 'y' or 'Y' for Yes");
+            Scanner playGameAgain = new Scanner(System.in);
+            usersResponse = playGameAgain.nextLine();
+        } while ((usersResponse.startsWith("y")) || (usersResponse.startsWith("Y")));
+    }
+}
