5ecd1f468d371829f25b511fb9bffb94bcbc3419
sidkrish03 <ss3875@njit.edu>
2021-03-11 13:16:58 -0500

    Initial Commit: Adding Coin class to get coin option, construct random coin option(Heads or Tails) for the player

---
+++ src/com/java/design/game/coinflip/Coin.java
@@ -0,0 +1,16 @@
+package com.java.design.game.coinflip;
+
+public class Coin {
+    private String coinOption = "";
+    public String[] coinValue = {"Heads", "Tails"};
+
+    Coin() {
+        // Random value of 0 or 1 is calculated
+        int randNum = (Math.random() < 0.5) ? 0 : 1;
+        coinOption = coinValue[randNum];
+    }
+
+    public String getCoinOption() {
+        return coinOption;
+    }
+}
