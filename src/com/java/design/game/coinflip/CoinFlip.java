653d9740a816ec8a95cff42af049c948a54e6f92
sidkrish03 <ss3875@njit.edu>
2021-03-11 13:20:58 -0500

    Initial Commit: Adding Coin Flip class to get player names, each player's coin option(Heads or Tails), results of each player

---
+++ src/com/java/design/game/coinflip/CoinFlip.java
@@ -0,0 +1,28 @@
+package com.java.design.game.coinflip;
+
+public class CoinFlip {
+    Player[] players = new Player[2];
+    Coin chosenCoin = new Coin();
+    CoinFlip(String player1Name, String player2Name) {
+        players[0] = new Player(player1Name);
+        players[1] = new Player(player2Name);
+    }
+
+    public void startGame() {
+        // Pick a random player to choose the face value guess
+        int randIndex = (Math.random() < 0.5) ? 0 : 1;
+        String playersPick = players[randIndex].getRandCoinOption();
+
+        // Set the opponents coinOption to the opposite value
+        int opponentsIndex = (randIndex == 0) ? 1 : 0;
+        players[opponentsIndex].setCoinOption(playersPick);
+
+        // Flip the coin to find the winning face value
+        String winningFlip = chosenCoin.getCoinOption();
+
+        // See the results of the flip
+        players[0].didPlayerWin(winningFlip);
+        players[1].didPlayerWin(winningFlip);
+    }
+}
+
